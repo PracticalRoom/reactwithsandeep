7ea3c071605ca28f7c5bd0c837ce5edc422df0bf
sanjeevpratapsingh <sanjeevpratapsingh93@gmail.com>
2019-03-25 07:46:04 +0530

    Adding One More Component in React JS

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component{
+    render(){
+        return(
+            <header>
+                <h1> Adding Up Some Data </h1>
+            </header>
+        );
+    }
+} 
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Header from './components/header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
+            <Header/>
             Hey I'm someone who has started developing the react application.
       </div>
     );
